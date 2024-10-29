ead78a3d7b98589848392f6bb5182e9c23e17e75
vanom <ji_yeon-_-@nate.com>
2024-10-29 23:56:16 +0900

    login 2

--- src/main/java/com/sparta/springauth/controller/UserController.java
+++ src/main/java/com/sparta/springauth/controller/UserController.java
@@ -1,15 +1,19 @@
 package com.sparta.springauth.controller;
 
+import com.sparta.springauth.dto.LoginRequestDto;
 import com.sparta.springauth.dto.SignupRequestDto;
 import com.sparta.springauth.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 @RequestMapping("/api")
 public class UserController {
+
 
     private final UserService userService;
 
@@ -33,4 +37,15 @@ public class UserController {
             return "redirect:/api/user/login-page";
         }
 
+
+        @PostMapping("/user/login")
+        public String login(LoginRequestDto requestDto, HttpServletResponse res ){
+            try {
+                userService.login(requestDto, res);
+            } catch (Exception e) {
+                return "redirect:/api/user/login-page?error";
+            }
+
+            return "redirect:/";
+        }
 }
