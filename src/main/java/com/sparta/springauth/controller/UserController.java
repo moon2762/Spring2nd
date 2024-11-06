d5cabc1e2e4fbaa9cb3620e79f4728a370687548
vanom <ji_yeon-_-@nate.com>
2024-11-06 20:20:50 +0900

    Validation 예외처리

--- src/main/java/com/sparta/springauth/controller/UserController.java
+++ src/main/java/com/sparta/springauth/controller/UserController.java
@@ -2,16 +2,21 @@ package com.sparta.springauth.controller;
 
 import com.sparta.springauth.dto.SignupRequestDto;
 import com.sparta.springauth.service.UserService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
 
+@Slf4j
 @Controller
 @RequestMapping("/api")
 public class UserController {
-
 
     private final UserService userService;
 
@@ -30,9 +35,19 @@ public class UserController {
     }
 
     @PostMapping("/user/signup")
-        public String signup (SignupRequestDto req){
-            userService.signup(req);
-            return "redirect:/api/user/login-page";
+    public String signup(@Valid SignupRequestDto requestDto, BindingResult bindingResult) {
+        // Validation 예외처리
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors(); // 오류가 난 필드 가져오기 (List)타입으로
+        if(fieldErrors.size() > 0) {
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                // fieldError.getField() 어떤 부분에서 오류가 나는지 알수있음
+                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+            }
+            return "redirect:/api/user/signup";
         }
 
+        userService.signup(requestDto);
+
+        return "redirect:/api/user/login-page";
+    }
 }
--- src/main/java/com/sparta/springauth/dto/ProductRequestDto.java
+++ src/main/java/com/sparta/springauth/dto/ProductRequestDto.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 
 @Getter
 public class ProductRequestDto {
-    @NotBlank
+    @NotBlank(message ="@@@")
     private String name;
     @Email
     private String email;
